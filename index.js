7879297bccec4fbe372807f3fb3ccdaa56b559c4
Mario Gentry-Humphrey <mario.gentry89@yahoo.com>
2022-06-27 19:05:43 -0500

    commit tree 9ac24ff6b151ee06b3bb10c871cee00ee0aa9c7a

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+function reverse(word) {
+  //const wordArr = word.split("");
+  //const reverseWordArr = wordArr.reverse();
+  //const reversedWord = reverseWordArr.join("");
+  return word.split("").reverse().join("")
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word)
+
+  if(word === reversedWord) {
+    return true
+  } else {
+    return false
+  }
+
+
 }
 
 /* 
   Add your pseudocode here
+
+  reverse the input string 
+
+  if the passed in string is the same as the string in reverse
+    return true
+
+    else return false
 */
 
 /*
@@ -20,6 +43,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log()
 }
 
 module.exports = isPalindrome;
